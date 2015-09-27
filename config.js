c41eb18bd67ccc0ef3e4f863a153c356ec8de822
Rob Eisenberg <rob@bluespire.com>
2015-09-27 18:47:21 -0400

    feat(all): update to use PAL

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ System.config({
 
   map: {
     "aurelia-logging": "github:aurelia/logging@0.7.0",
+    "aurelia-pal": "github:aurelia/pal@0.1.1",
     "babel": "npm:babel-core@5.8.23",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@0.9.18",
--- test/console-appender.spec.js
+++ test/console-appender.spec.js
@@ -1,8 +1,27 @@
 import {ConsoleAppender} from '../src/index';
 
 describe('console appender', () => {
-  it('can be instantiated', () => {
-    var appender = new ConsoleAppender();
-    expect(appender).not.toBe(null);
+  let logger;
+  let appender;
+
+  beforeEach(() => {
+    logger = {id:'test logger'};
+    appender = new ConsoleAppender();
+  });
+
+  it('can debug', () => {
+    appender.debug(logger, 'test debug message');
+  });
+
+  it('can warn', () => {
+    appender.warn(logger, 'test warn message');
+  });
+
+  it('can info', () => {
+    appender.info(logger, 'test info message');
+  });
+
+  it('can error', () => {
+    appender.error(logger, 'test error message');
   });
 });
